a863ed5f7a919b37777238261614c08d6ca22e1d
Erfan nasiri <164219925+Erfan-ns@users.noreply.github.com>
2024-06-07 13:14:48 +0330

    Update hads_game.py

--- hads_game.py
+++ hads_game.py
@@ -1,9 +1,9 @@
 import random
-javab = random.randint(1, 100)
+javab = random.randint(1 , 100)
 hads = input("hadset chie? ")
 hads = int(hads)
-while hads != javab:
-    print("hadset balas mashti!") if hads > javab else print("hadset paeene mashti!")
-    hads = input("hadset chie? ")
+while hads!= javab:
+    print("hadset paeene mashti! \nboro bala tar") if hads < javab else print("hadset balas mashti! \nboro paeen tar")
+    hads = input("dobare hads bezan ")
     hads = int(hads)
-print("ay barikala")
+print("ahaa hala shod")
